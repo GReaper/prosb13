ef182eb694d8c5f568773be5f6fa700629800e7e
Xaby <ltreap.face@gmail.com>
2013-05-25 12:46:33 +0200

    Generados movimientos basicos para las 3 posibles situaciones: ganar, perder o empatar

--- ISBCTournament/ISBCTournament/src/AstonBirrasFc/Entrenador.java
+++ ISBCTournament/ISBCTournament/src/AstonBirrasFc/Entrenador.java
@@ -26,8 +26,11 @@ public class Entrenador extends TeamManager
 		 */
 		myRobotAPI = _players[0].getRobotAPI();
 		
-		/* Vamos ganando o empatados */
-		if (myRobotAPI.getMyScore() >= myRobotAPI.getOpponentScore())
+		/* Vamos empatados
+		 *  
+		 *  Comportamiento normal
+		 */
+		if (myRobotAPI.getMyScore() == myRobotAPI.getOpponentScore())
 		{
 			// Cambiar portero por MC si es necesario
 			if (_players[0].getRobotAPI().opponentBlocking())
@@ -77,7 +80,64 @@ public class Entrenador extends TeamManager
 			}
 		}
 		
-		/* Vamos perdiendo */
+		/* Vamos ganando 
+		 *  
+		 *  Agregamos un pepe o bloqueador atrás para defender más
+		 */
+		else if (myRobotAPI.getMyScore() == myRobotAPI.getOpponentScore())
+		{
+			// Cambiar portero por MC si es necesario
+			if (_players[0].getRobotAPI().opponentBlocking())
+			{
+				_players[0].setBehaviour(_behaviours[5]);
+				_players[1].setBehaviour(_behaviours[7]);				
+			}
+			// Comprobar cambios en el MC. El portero no está bloqueado y no es necesario
+			// cambiarlo.
+			else
+			{
+				_players[0].setBehaviour(_behaviours[7]);
+				_players[1].setBehaviour(_behaviours[5]);				
+			}	
+
+			// Si el desmarcador "inicial" tiene cerca la bola, el delantero pasa a 
+			// bloquear al portero y el desm. a regateador
+			if (helper.cercanoRadio(_players[4].getRobotAPI().getPosition(), 
+									_players[4].getRobotAPI().toFieldCoordinates(_players[4].getRobotAPI().getBall()), 
+									_players[4].getRobotAPI().getPlayerRadius()*3))
+			{
+				_players[2].setBehaviour(_behaviours[2]);
+				_players[3].setBehaviour(_behaviours[8]);
+				_players[4].setBehaviour(_behaviours[6]);
+				
+			}
+			// Si es al revés, que el delantero "inicial" es quien tiene cerca la bola,
+			// éste pasa a regateador y el desmarcador a bloqueador
+			else if (helper.cercanoRadio(_players[3].getRobotAPI().getPosition(), 
+						_players[3].getRobotAPI().toFieldCoordinates(_players[4].getRobotAPI().getBall()), 
+						_players[3].getRobotAPI().getPlayerRadius()*3))
+			{
+				_players[2].setBehaviour(_behaviours[2]);
+				_players[3].setBehaviour(_behaviours[6]);
+				_players[4].setBehaviour(_behaviours[8]);
+				
+			}
+			// Si no se da ninguno de los casos, el MCO "inicial" pasa a Pepe
+			// o reboteador
+			else
+			{
+				// TODO: falta el jugador PEPE!
+				// Cambio a reboteador.
+				_players[2].setBehaviour(_behaviours[3]);
+				_players[3].setBehaviour(_behaviours[9]);
+				_players[4].setBehaviour(_behaviours[4]);
+			}
+		}
+		
+		/* Vamos perdiendo 
+		 *  
+		 *  
+		 */
 		else
 		{
 			// Cambiar portero por MC si es necesario
@@ -92,7 +152,40 @@ public class Entrenador extends TeamManager
 			{
 				_players[0].setBehaviour(_behaviours[7]);
 				_players[1].setBehaviour(_behaviours[5]);				
-			}		
+			}	
+
+			// Si el desmarcador "inicial" tiene cerca la bola, el delantero pasa a 
+			// bloquear al portero y el desm. a regateador
+			if (helper.cercanoRadio(_players[4].getRobotAPI().getPosition(), 
+									_players[4].getRobotAPI().toFieldCoordinates(_players[4].getRobotAPI().getBall()), 
+									_players[4].getRobotAPI().getPlayerRadius()*3))
+			{
+				_players[2].setBehaviour(_behaviours[2]);
+				_players[3].setBehaviour(_behaviours[8]);
+				_players[4].setBehaviour(_behaviours[6]);
+				
+			}
+			// Si es al revés, que el delantero "inicial" es quien tiene cerca la bola,
+			// éste pasa a regateador y el desmarcador a bloqueador
+			else if (helper.cercanoRadio(_players[3].getRobotAPI().getPosition(), 
+						_players[3].getRobotAPI().toFieldCoordinates(_players[4].getRobotAPI().getBall()), 
+						_players[3].getRobotAPI().getPlayerRadius()*3))
+			{
+				_players[2].setBehaviour(_behaviours[2]);
+				_players[3].setBehaviour(_behaviours[6]);
+				_players[4].setBehaviour(_behaviours[8]);
+				
+			}
+			// Si no se da ninguno de los casos, el MCO "inicial" pasa a Pepe
+			// o reboteador
+			else
+			{
+				// TODO: falta el jugador PEPE!
+				// Cambio a reboteador.
+				_players[2].setBehaviour(_behaviours[3]);
+				_players[3].setBehaviour(_behaviours[9]);
+				_players[4].setBehaviour(_behaviours[4]);
+			}
 		}		
 	}
 	
