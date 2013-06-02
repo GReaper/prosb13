7171868716f1a3aaed8fdfd46e41a9713bc4e874
Reaper <reap4rep@gmail.com>
2013-06-02 10:35:51 +0200

    Cambiado bloqueador a estados

--- ISBCTournament/ISBCTournament/src/AstonBirrasFc/BloqueaPortero.java
+++ ISBCTournament/ISBCTournament/src/AstonBirrasFc/BloqueaPortero.java
@@ -7,76 +7,73 @@ import teams.ucmTeam.RobotAPI;
 public class BloqueaPortero extends Behaviour
 {
 	private Ayudas helper;
+	private enum States {AvoidFrBlock, AvoidOppBlock, AvoidOpp, ToGoal, Blocking};
+	private States state;
 	
 	public void configure() 
 	{ 
 		helper = new Ayudas();
+		state = States.ToGoal;
 	}
 
 	public int takeStep() 
 	{	
-		// Avoid collisions only if the opponents goal is far enough
-		if (!helper.cercanoRadio(myRobotAPI.getPosition(), helper.porteroEnemigo(myRobotAPI), myRobotAPI.getPlayerRadius()*3))
+		// Evaluate environment
+		state = evaluateEnv();
+		
+		// Take closest opponent
+		Vec2 closestOp = myRobotAPI.getClosestOpponent();
+		
+		// Make movement based on state
+		switch (state) 
 		{
-			// Take closest opponent
-			Vec2 closestOp = myRobotAPI.getClosestOpponent();
-			
-			// Check whether the player is blocked by the closest teammate
-			if (myRobotAPI.isBlocking(myRobotAPI.getClosestMate()))
-			{
-				helper.evitarBloqueo(myRobotAPI.getClosestMate(), myRobotAPI);
-	
+			case AvoidFrBlock:	
+				// Avoid block
+				helper.evitarBloqueo(myRobotAPI.getClosestMate(), myRobotAPI);	
 				// Set displayed text
-				myRobotAPI.setDisplayString("BloqPort. (AF)");
-
-				// Return
-				return myRobotAPI.ROBOT_OK;
-			}
-
-			// Check for opponent blocking
-			if (myRobotAPI.opponentBlocking())
-			{
-				helper.evitarBloqueo(closestOp, myRobotAPI);
-	
-				// Set displayed text
-				myRobotAPI.setDisplayString("BloqPort. (AO2)");
+				myRobotAPI.setDisplayString("BloqPort. (AF)");		
+				break;
 				
-				// Return
-				return myRobotAPI.ROBOT_OK;				
-			}
-			
-			// Check whether that opponent is too close
-			if (helper.cercanoRadio(myRobotAPI.getPosition(),  myRobotAPI.toFieldCoordinates(closestOp), myRobotAPI.getPlayerRadius()*2))
-			{
+			case AvoidOppBlock:	
+				// Avoid block
+				helper.evitarBloqueo(closestOp, myRobotAPI);	
+				// Set displayed text
+				myRobotAPI.setDisplayString("BloqPort. (AO2)");							
+				break;
+				
+			case AvoidOpp:	
+				// Avoid collision
 				helper.evitaColision(closestOp, myRobotAPI);
-
 				// Set displayed text
-				myRobotAPI.setDisplayString("BloqPort. (AO1)");
+				myRobotAPI.setDisplayString("BloqPort. (AO1)");							
+				break;
 				
-				// Return
-				return myRobotAPI.ROBOT_OK;
-			}
-			
-			// 1.- Set steering
-			Vec2 dest = myRobotAPI.toFieldCoordinates(myRobotAPI.getOpponentsGoal());//helper.porteroEnemigo(myRobotAPI);
-			double angle = helper.anguloDestino(dest, myRobotAPI);
-			myRobotAPI.setSteerHeading(angle);	
-
-			// 2.- Increase speed
-			myRobotAPI.setSpeed(1000);
-			
-			// Set displayed text
-			myRobotAPI.setDisplayString("BloqPort. (GTG)");
-			 
-			return myRobotAPI.ROBOT_OK;
+			case ToGoal:	
+				// 1.- Set steering
+				Vec2 dest = myRobotAPI.toFieldCoordinates(myRobotAPI.getOpponentsGoal());
+				double angle = helper.anguloDestino(dest, myRobotAPI);
+				myRobotAPI.setSteerHeading(angle);
+				// 2.- Increase speed
+				myRobotAPI.setSpeed(1000);				
+				// Set displayed text
+				myRobotAPI.setDisplayString("BloqPort. (GTG)");			
+				break;
+				
+			case Blocking:	
+				// Move towards opponents goalie
+				myRobotAPI.blockGoalKeeper();				
+				// Set displayed text
+				myRobotAPI.setDisplayString("BloqPort. (B)");			
+				break;
+				
+			default:
+				// Move towards opponents goalie
+				myRobotAPI.blockGoalKeeper();				
+				// Set displayed text
+				myRobotAPI.setDisplayString("BloqPort. (B)");
+				break;
 		}
 		
-		// Move towards opponents goalie
-		myRobotAPI.blockGoalKeeper();
-		
-		// Set displayed text
-		myRobotAPI.setDisplayString("BloqPort. (B)");
-		 
 		return myRobotAPI.ROBOT_OK;
 	}
 
@@ -87,4 +84,41 @@ public class BloqueaPortero extends Behaviour
 	public void onRelease(RobotAPI r) { }
 
 	public void end() { }
+	
+	private States evaluateEnv()
+	{
+		// Avoid collisions only if the opponents goal is far enough
+		if (!helper.cercanoRadio(myRobotAPI.getPosition(), helper.porteroEnemigo(myRobotAPI), myRobotAPI.getPlayerRadius()*3))
+		{
+			// Take closest opponent
+			Vec2 closestOp = myRobotAPI.getClosestOpponent();
+			
+			// Check whether the player is blocked by the closest teammate
+			if (myRobotAPI.isBlocking(myRobotAPI.getClosestMate()))
+			{
+				// Return
+				return States.AvoidFrBlock;
+			}
+
+			// Check for opponent blocking
+			if (myRobotAPI.opponentBlocking())
+			{
+				// Return
+				return States.AvoidOppBlock;				
+			}
+			
+			// Check whether that opponent is too close
+			if (helper.cercanoRadio(myRobotAPI.getPosition(),  myRobotAPI.toFieldCoordinates(closestOp), myRobotAPI.getPlayerRadius()*2))
+			{
+				// Return
+				return States.AvoidOpp;
+			}
+			
+			// Move towards enemy goal
+			return States.ToGoal;
+		}
+		
+		// Move towards opponents goalie
+		return States.Blocking;
+	}
 }
