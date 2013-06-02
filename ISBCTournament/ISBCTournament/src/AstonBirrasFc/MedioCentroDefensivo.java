d4ee4598229b9be5eaeb271a43b5fb79262c4cad
Reaper <reap4rep@gmail.com>
2013-06-02 10:59:06 +0200

    Cambiado MCD a maquina de estados

--- ISBCTournament/ISBCTournament/src/AstonBirrasFc/MedioCentroDefensivo.java
+++ ISBCTournament/ISBCTournament/src/AstonBirrasFc/MedioCentroDefensivo.java
@@ -7,89 +7,18 @@ import teams.ucmTeam.RobotAPI;
 public class MedioCentroDefensivo extends Behaviour
 {
 	private Ayudas helper;
+	private enum States {AvoidFrBlock, MoveMiddle, MoveThird, FollowBall, Kicking, GoToBall};
+	private States state;
 	
 	public void configure() 
 	{ 
 		helper = new Ayudas();
+		state = States.MoveThird;
 	}
 
 	public int takeStep() 
 	{		
-		// Check whether the player is blocked by the closest teammate
-		if (myRobotAPI.isBlocking(myRobotAPI.getClosestMate()))
-		{
-			helper.evitarBloqueo(myRobotAPI.getClosestMate(), myRobotAPI);
-			
-			// Set displayed text
-			myRobotAPI.setDisplayString("MCD (AF)");
-			
-			// Return
-			return myRobotAPI.ROBOT_OK;
-		}
-		
-		// If the ball is in our field the MCD can move until the middle section,
-		// else he may only remain in the first third section
-		if (myRobotAPI.toFieldCoordinates(myRobotAPI.getBall()).x * myRobotAPI.getFieldSide() >= 0)
-		{
-			// Check if the player is in the first middle of the field
-			if (!(myRobotAPI.getPosition().x * myRobotAPI.getFieldSide() >= 0))
-			{
-				// 1.- Set steering
-				Vec2 dest = myRobotAPI.getOurGoal();
-				double angle = helper.anguloDestino(dest, myRobotAPI);			
-				myRobotAPI.setSteerHeading(angle);	
-				
-				// 2.- Increase speed
-				myRobotAPI.setSpeed(1000);
-
-				// Set displayed text
-				myRobotAPI.setDisplayString("MCD (RM)");
-				
-				// Return
-				return myRobotAPI.ROBOT_OK;
-			}
-		}
-		else
-		{
-			Double thirdSize = (myRobotAPI.getRightFieldBound() - myRobotAPI.getLeftFieldBound()) / 3;
-			// Check if the player is in the first third of the field
-			if (!(myRobotAPI.getPosition().x * myRobotAPI.getFieldSide() >= thirdSize))
-			{
-				// 1.- Set steering
-				Vec2 dest = myRobotAPI.getOurGoal();
-				double angle = helper.anguloDestino(dest, myRobotAPI);			
-				myRobotAPI.setSteerHeading(angle);	
-				
-				// 2.- Increase speed
-				myRobotAPI.setSpeed(1000);
-
-				// Set displayed text
-				myRobotAPI.setDisplayString("MCD (RT)");
-				
-				// Return
-				return myRobotAPI.ROBOT_OK;
-			}
-			else
-			{
-				// Move following the ball
-
-				// 1.- Set steering
-				Vec2 dest = myRobotAPI.toFieldCoordinates(myRobotAPI.getBall());
-				double angle = helper.anguloDestino(dest, myRobotAPI);			
-				myRobotAPI.setSteerHeading(angle);	
-				
-				// 2.- Increase speed
-				myRobotAPI.setSpeed(0.2);
-
-				// Set displayed text
-				myRobotAPI.setDisplayString("MCD (FB2)");
-				
-				// Return
-				return myRobotAPI.ROBOT_OK;
-			}
-		}
-		
-		// Decide the side to kick the ball
+		// Decide the side to kick the ball in case it's needed
 		Vec2 dest = myRobotAPI.getOpponentsGoal();
 		// Decide where to kick the ball
 		if (myRobotAPI.getPosition().y > 0)
@@ -103,23 +32,78 @@ public class MedioCentroDefensivo extends Behaviour
 			dest = new Vec2(0, myRobotAPI.getLowerFieldBound());				
 		}
 		
-		// Check if the player can kick the ball
-		if (myRobotAPI.canKick())
+		// Evaluate environment
+		state = evaluateEnv();
+		
+		// Make movements
+		switch (state) 
 		{
-			myRobotAPI.setBehindBall(myRobotAPI.toEgocentricalCoordinates(dest));
-			myRobotAPI.kick();
-			
-			// Set displayed text
-			myRobotAPI.setDisplayString("MCD (K)");
-		}
-		
-		// Go to ball to follow it
-		myRobotAPI.setSpeed(1000);
-		
-		myRobotAPI.setBehindBall(myRobotAPI.toEgocentricalCoordinates(dest));
+			case AvoidFrBlock:
+				// Avoid block
+				helper.evitarBloqueo(myRobotAPI.getClosestMate(), myRobotAPI);				
+				// Set displayed text
+				myRobotAPI.setDisplayString("MCD (AF)");								
+				break;
 
-		// Set displayed text
-		myRobotAPI.setDisplayString("MCD (FB)");
+			case MoveMiddle:
+				// 1.- Set steering
+				dest = myRobotAPI.getOurGoal();
+				double angle = helper.anguloDestino(dest, myRobotAPI);			
+				myRobotAPI.setSteerHeading(angle);					
+				// 2.- Increase speed
+				myRobotAPI.setSpeed(1000);
+				// Set displayed text
+				myRobotAPI.setDisplayString("MCD (RM)");								
+				break;
+
+			case MoveThird:	
+				// 1.- Set steering
+				dest = myRobotAPI.getOurGoal();
+				double angle2 = helper.anguloDestino(dest, myRobotAPI);			
+				myRobotAPI.setSteerHeading(angle2);					
+				// 2.- Increase speed
+				myRobotAPI.setSpeed(1000);
+				// Set displayed text
+				myRobotAPI.setDisplayString("MCD (RT)");							
+				break;
+
+			case FollowBall:	
+				// 1.- Set steering
+				dest = myRobotAPI.toFieldCoordinates(myRobotAPI.getBall());
+				double angle3 = helper.anguloDestino(dest, myRobotAPI);			
+				myRobotAPI.setSteerHeading(angle3);					
+				// 2.- Increase speed
+				myRobotAPI.setSpeed(0.2);
+				// Set displayed text
+				myRobotAPI.setDisplayString("MCD (FB2)");							
+				break;
+
+			case Kicking:					
+				myRobotAPI.setBehindBall(myRobotAPI.toEgocentricalCoordinates(dest));
+				myRobotAPI.kick();					
+				// Set displayed text
+				myRobotAPI.setDisplayString("MCD (K)");		
+				break;
+
+			case GoToBall:		
+				// Go to ball to follow it
+				myRobotAPI.setSpeed(1000);				
+				myRobotAPI.setBehindBall(myRobotAPI.toEgocentricalCoordinates(dest));
+				// Set displayed text
+				myRobotAPI.setDisplayString("MCD (FB)");				
+				break;
+	
+			default:	
+				// 1.- Set steering
+				dest = myRobotAPI.getOurGoal();
+				double angle4 = helper.anguloDestino(dest, myRobotAPI);			
+				myRobotAPI.setSteerHeading(angle4);					
+				// 2.- Increase speed
+				myRobotAPI.setSpeed(1000);
+				// Set displayed text
+				myRobotAPI.setDisplayString("MCD (RT)");
+				break;
+		}
 		
 		return myRobotAPI.ROBOT_OK;
 	}
@@ -131,4 +115,51 @@ public class MedioCentroDefensivo extends Behaviour
 	public void onRelease(RobotAPI r) { }
 
 	public void end() { }
+	
+	private States evaluateEnv()
+	{
+		// Check whether the player is blocked by the closest teammate
+		if (myRobotAPI.isBlocking(myRobotAPI.getClosestMate()))
+		{
+			// Return
+			return States.AvoidFrBlock;
+		}
+		
+		// If the ball is in our field the MCD can move until the middle section,
+		// else he may only remain in the first third section
+		if (myRobotAPI.toFieldCoordinates(myRobotAPI.getBall()).x * myRobotAPI.getFieldSide() >= 0)
+		{
+			// Check if the player is in the first middle of the field
+			if (!(myRobotAPI.getPosition().x * myRobotAPI.getFieldSide() >= 0))
+			{
+				// Return
+				return States.MoveMiddle;
+			}
+		}
+		else
+		{
+			Double thirdSize = (myRobotAPI.getRightFieldBound() - myRobotAPI.getLeftFieldBound()) / 3;
+			// Check if the player is in the first third of the field
+			if (!(myRobotAPI.getPosition().x * myRobotAPI.getFieldSide() >= thirdSize))
+			{
+				// Return
+				return States.MoveThird;
+			}
+			else
+			{
+				// Move following the ball
+				// Return
+				return States.FollowBall;
+			}
+		}
+		
+		// Check if the player can kick the ball
+		if (myRobotAPI.canKick())
+		{
+			return States.Kicking;
+		}
+		
+		// Go to ball to follow it
+		return States.GoToBall;		
+	}
 }
