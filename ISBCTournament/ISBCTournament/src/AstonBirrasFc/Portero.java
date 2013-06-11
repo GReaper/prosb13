7ebf412f6bf2ab50d90e60bc3a682a3212497a0d
Roberto Marin <ka2rober@gmail.com>
2013-06-11 11:05:40 +0200

    Cambiado el portero a maquina de estados

--- ISBCTournament/ISBCTournament/src/AstonBirrasFc/Portero.java
+++ ISBCTournament/ISBCTournament/src/AstonBirrasFc/Portero.java
@@ -1,5 +1,6 @@
 package AstonBirrasFc;
 
+
 import EDU.gatech.cc.is.util.Vec2;
 import teams.ucmTeam.Behaviour;
 import teams.ucmTeam.RobotAPI;
@@ -8,6 +9,9 @@ public class Portero extends Behaviour
 {
 	private static int SIDE;
 	private Ayudas ayuda;
+	private enum Estados {DesbloqCOM, DesbloqCON,MoverPortero,PelotaAtrasGolpear,FueraAreaVuelve,Porteria};
+	private Estados estado;
+	
 	public void configure() 
 	{ 
 		ayuda=new Ayudas();
@@ -15,12 +19,146 @@ public class Portero extends Behaviour
 
 	public int takeStep() 
 	{				
-		//Cojo posicion de la bola
+		estado=calculaEstado();
+		
 		Vec2 ball=myRobotAPI.getBall();
-		//Cojo donde esta mi porteria
-		Vec2 ourgoal=myRobotAPI.getOurGoal();
 		Vec2[] teammates=myRobotAPI.getTeammates();
 		Vec2[] opponents=myRobotAPI.getOpponents();
+		Vec2 ourgoal=myRobotAPI.getOurGoal();
+		SIDE=myRobotAPI.getFieldSide();
+		
+		switch (estado) {
+			case DesbloqCOM:
+				ayuda.evitarBloqueo(myRobotAPI.getClosestMate(), myRobotAPI);
+				myRobotAPI.setDisplayString("Por. (EBC)");
+				
+				//Evita colision compañeros
+				for(int i =0;i<teammates.length;i++) 
+				{
+					if(ayuda.cercanoRadio(myRobotAPI.getPosition(), myRobotAPI.toFieldCoordinates(teammates[i]),0.2))
+					{
+						ayuda.evitaColision(myRobotAPI.toFieldCoordinates(teammates[i]), myRobotAPI);
+						myRobotAPI.setDisplayString("Portero (ECC)");
+					}
+				}
+				//Evita colision oponentes
+				for(int i =0;i<opponents.length;i++)
+				{
+					if(ayuda.cercanoRadio(myRobotAPI.getPosition(), myRobotAPI.toFieldCoordinates(opponents[i]),0.2))
+					{
+						ayuda.evitaColision(myRobotAPI.toFieldCoordinates(opponents[i]), myRobotAPI);
+						myRobotAPI.setDisplayString("Portero (ECO)");
+					}
+				}
+				
+				break;
+				
+			case DesbloqCON:
+				ayuda.evitarBloqueo(myRobotAPI.getClosestOpponent(), myRobotAPI);
+				myRobotAPI.setDisplayString("Por. (EBO)");
+				
+				//Evita colision compañeros
+				for(int i =0;i<teammates.length;i++) 
+				{
+					if(ayuda.cercanoRadio(myRobotAPI.getPosition(), myRobotAPI.toFieldCoordinates(teammates[i]),0.2))
+					{
+						ayuda.evitaColision(myRobotAPI.toFieldCoordinates(teammates[i]), myRobotAPI);
+						myRobotAPI.setDisplayString("Portero (ECC)");
+					}
+				}
+				//Evita colision Oponentes
+				for(int i =0;i<opponents.length;i++)
+				{
+					if(ayuda.cercanoRadio(myRobotAPI.getPosition(), myRobotAPI.toFieldCoordinates(opponents[i]),0.2))
+					{
+						ayuda.evitaColision(myRobotAPI.toFieldCoordinates(opponents[i]), myRobotAPI);
+						myRobotAPI.setDisplayString("Portero (ECO)");
+					}
+				}
+				break;
+				
+			case MoverPortero:
+				myRobotAPI.setSteerHeading(ayuda.irAPosicionParando(myRobotAPI.toFieldCoordinates(ourgoal), myRobotAPI,0.01));
+				myRobotAPI.setSpeed(1.0);
+				myRobotAPI.setDisplayString("Portero (area)");
+				break;
+				
+			case PelotaAtrasGolpear:
+				// Decide hacia que lado golpear el balon
+				Vec2 dest = myRobotAPI.getOpponentsGoal(); 
+				// decide donde golpea la bola
+				if (myRobotAPI.getPosition().y > 0)
+				{
+					// Golpea a la parte superior
+					dest = new Vec2(0, myRobotAPI.getUpperFieldBound());
+				}
+				else
+				{
+					// Golpea a la parte inferior
+					dest = new Vec2(0, myRobotAPI.getLowerFieldBound());				
+				}
+				
+				// Comprueba si puede golpear la bola
+				if (myRobotAPI.canKick())
+				{
+					myRobotAPI.setBehindBall(myRobotAPI.toEgocentricalCoordinates(dest));
+					myRobotAPI.kick();
+					myRobotAPI.setDisplayString("Portero (kick)");
+				}
+				break;
+				
+			case FueraAreaVuelve:
+				Vec2 move;
+				if (ourgoal.y < 0)
+				{
+					move=new Vec2((double)SIDE, -8);				
+				}
+				else
+				{
+					move=new Vec2((double)SIDE, 8);				
+				}
+				myRobotAPI.setSteerHeading(ayuda.irAPosicionParando(myRobotAPI.toFieldCoordinates(move), myRobotAPI,0.01));
+				//myRobotAPI.setSpeed(1.0);
+				myRobotAPI.setDisplayString("Portero (area)");
+				break;
+				
+			case Porteria:
+				Vec2 movePor;
+				//Para que este entre el balon y la porteria
+				if( ball.y > 0)
+				{
+					movePor=new Vec2((double)SIDE, 8);
+					myRobotAPI.setSteerHeading(ayuda.irAPosicionParando(myRobotAPI.toFieldCoordinates(movePor), myRobotAPI,0.0001));
+				}
+				else
+				{
+					movePor=new Vec2((double)SIDE, -8);
+					myRobotAPI.setSteerHeading(ayuda.irAPosicionParando(myRobotAPI.toFieldCoordinates(movePor), myRobotAPI,0.0001));
+				}
+
+				if( Math.abs( ball.y) < myRobotAPI.getPlayerRadius() * 0.15)
+				{
+					myRobotAPI.setSpeed(0);
+				}				
+				else
+				{
+					myRobotAPI.setSpeed(1.0);
+				}			
+				myRobotAPI.setDisplayString("Portero (wait)");
+				
+				break;
+				
+			default:
+				break;
+		}
+		return myRobotAPI.ROBOT_OK;
+	}
+
+	
+	private Estados calculaEstado()
+	{
+		//Cojo posicion de la bola
+		Vec2 ball=myRobotAPI.getBall();
 		SIDE=myRobotAPI.getFieldSide();
 		//if(distancia > 0.18)
 		// Tomar seccion % del campo total
@@ -32,115 +170,36 @@ public class Portero extends Behaviour
 			//Si esta bloqueado se zafa de el(amigo)
 			if (myRobotAPI.isBlocking(myRobotAPI.getClosestMate()))
 			{
-				ayuda.evitarBloqueo(myRobotAPI.getClosestMate(), myRobotAPI);
-				myRobotAPI.setDisplayString("Por. (EBC)");
+				return Estados.DesbloqCOM;
 			}
 			
 			//Si esta bloqueado se zafa de el(Oponente)
 			if (myRobotAPI.isBlocking(myRobotAPI.getClosestOpponent()))
 			{
-				ayuda.evitarBloqueo(myRobotAPI.getClosestOpponent(), myRobotAPI);
-				myRobotAPI.setDisplayString("Por. (EBO)");
-			}
-			
-			//Evita colision compañeros
-			for(int i =0;i<teammates.length;i++) 
-			{
-				if(ayuda.cercanoRadio(myRobotAPI.getPosition(), myRobotAPI.toFieldCoordinates(teammates[i]),0.2))
-				{
-					ayuda.evitaColision(myRobotAPI.toFieldCoordinates(teammates[i]), myRobotAPI);
-					myRobotAPI.setDisplayString("Portero (ECC)");
-				}
-			}
-			
-			for(int i =0;i<opponents.length;i++)
-			{
-				if(ayuda.cercanoRadio(myRobotAPI.getPosition(), myRobotAPI.toFieldCoordinates(opponents[i]),0.2))
-				{
-					ayuda.evitaColision(myRobotAPI.toFieldCoordinates(opponents[i]), myRobotAPI);
-					myRobotAPI.setDisplayString("Portero (ECO)");
-				}
-			}	
+				return Estados.DesbloqCON;
+			}				
+				
 			if( (myRobotAPI.getPosition().x * SIDE < 1.37 || Math.abs(myRobotAPI.getPosition().y) > 0.25))
 			{
-				myRobotAPI.setSteerHeading(ayuda.irAPosicionParando(myRobotAPI.toFieldCoordinates(ourgoal), myRobotAPI,0.01));
-				myRobotAPI.setSpeed(1.0);
-				myRobotAPI.setDisplayString("Portero (area)");
+				return Estados.MoverPortero;
 			}
 			myRobotAPI.setDisplayString("Portero (Out)");
 		}
-		else
+		
 		//Si la pelota esta detras del portero intenta ir a por ella y golpearla hacia fuera
 		if( ball.x * SIDE > 0)
 		{
-			// Decide hacia que lado golpear el balon
-			Vec2 dest = myRobotAPI.getOpponentsGoal(); 
-			// decide donde golpea la bola
-			if (myRobotAPI.getPosition().y > 0)
-			{
-				// Golpea a la parte superior
-				dest = new Vec2(0, myRobotAPI.getUpperFieldBound());
-			}
-			else
-			{
-				// Golpea a la parte inferior
-				dest = new Vec2(0, myRobotAPI.getLowerFieldBound());				
-			}
-			
-			// Comprueba si puede golpear la bola
-			if (myRobotAPI.canKick())
-			{
-				myRobotAPI.setBehindBall(myRobotAPI.toEgocentricalCoordinates(dest));
-				myRobotAPI.kick();
-				myRobotAPI.setDisplayString("Portero (kick)");
-			}
-			myRobotAPI.setDisplayString("Portero (back)");
+			return Estados.PelotaAtrasGolpear;
 		}
-		else
+		
 		//Si esta fuera del area de gol vuelve
 		if( (myRobotAPI.getPosition().x * SIDE < 1.07 || Math.abs(myRobotAPI.getPosition().y) > 0.2222))
 		{
-			Vec2 move;
-			if (ourgoal.y < 0)
-			{
-				move=new Vec2((double)SIDE, -8);				
-			}
-			else
-			{
-				move=new Vec2((double)SIDE, 8);				
-			}
-			myRobotAPI.setSteerHeading(ayuda.irAPosicionParando(myRobotAPI.toFieldCoordinates(move), myRobotAPI,0.01));
-			//myRobotAPI.setSpeed(1.0);
-			myRobotAPI.setDisplayString("Portero (area)");
+			return Estados.FueraAreaVuelve;
 		}
-		else		
-		{
-			//Para que este entre el balon y la porteria
-			if( ball.y > 0)
-			{
-				Vec2 move=new Vec2((double)SIDE, 8);
-				myRobotAPI.setSteerHeading(ayuda.irAPosicionParando(myRobotAPI.toFieldCoordinates(move), myRobotAPI,0.0001));
-			}
-			else
-			{
-				Vec2 move=new Vec2((double)SIDE, -8);
-				myRobotAPI.setSteerHeading(ayuda.irAPosicionParando(myRobotAPI.toFieldCoordinates(move), myRobotAPI,0.0001));
-			}
-
-			if( Math.abs( ball.y) < myRobotAPI.getPlayerRadius() * 0.15)
-			{
-				myRobotAPI.setSpeed(0);
-			}				
-			else
-			{
-				myRobotAPI.setSpeed(1.0);
-			}			
-			myRobotAPI.setDisplayString("Portero (wait)");
-				
-		}
-		return myRobotAPI.ROBOT_OK;
+		return Estados.Porteria;
 	}
-
+	
 	public void onInit(RobotAPI r) 
 	{
 	}
