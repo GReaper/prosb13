ff61a2d2ba0eedfbe97b67181ead8b97352a77db
Roberto Marin <ka2rober@gmail.com>
2013-06-11 11:54:16 +0200

    Pasado pepe y el delantero a maquina de estados

--- ISBCTournament/ISBCTournament/src/AstonBirrasFc/Delantero.java
+++ ISBCTournament/ISBCTournament/src/AstonBirrasFc/Delantero.java
@@ -7,6 +7,8 @@ import EDU.gatech.cc.is.util.Vec2;
 public class Delantero extends Behaviour
 {
 	private Ayudas ayuda;
+	private enum Estados {Golpear,DetrasPelota,PasaBola,EsperarBola};
+	private Estados estado;
 	
 	public void configure() 
 	{ 
@@ -14,50 +16,20 @@ public class Delantero extends Behaviour
 	}
 
 	public int takeStep() 
-	{				
-		//Cojo posicion de la bola
-		Vec2 ball=myRobotAPI.getBall();
-		Vec2 ballCoordinates=myRobotAPI.toFieldCoordinates(ball);
-		int defenseside=myRobotAPI.getFieldSide();
-		int ataqueside=(-1)*defenseside;
-		//ayuda.evitaColision(myRobotAPI.getClosestMate(), myRobotAPI);
-
-		//Si la pelota esta en el lado donde se encuentra el delantero va a por la pelota
-		if( (ballCoordinates.x >= 0 && ataqueside==myRobotAPI.EAST_FIELD) || (ballCoordinates.x <= 0 && ataqueside==myRobotAPI.WEST_FIELD))
-		{
-			myRobotAPI.setDisplayString("Del. (PK)");
-			//Si puede golpearla, la golpea
-			//Sino prueba a pasarla
-			if(ayuda.cercano(myRobotAPI.getPosition(), myRobotAPI.toFieldCoordinates(myRobotAPI.getOpponentsGoal()), myRobotAPI, 0.25)
-					&& myRobotAPI.canKick() /*&& myRobotAPI.alignedToBallandGoal()*/
-					&& !ayuda.cercanoRadio(myRobotAPI.getPosition(),  myRobotAPI.toFieldCoordinates(myRobotAPI.getClosestOpponent()), myRobotAPI.getPlayerRadius()*2))
-			{
-				//myRobotAPI.setBehindBall(ball);
-				//myRobotAPI.setBehindBall(myRobotAPI.getOpponentsGoal());
+	{		
+		estado=calculaEstado();
+		
+		switch (estado) {
+			case Golpear:
+				myRobotAPI.setDisplayString("Del. (Tiro)");
+				myRobotAPI.kick();		
+				break;
 				
-				//Vec2 goal = myRobotAPI.toFieldCoordinates(myRobotAPI.getOpponentsGoal());
-				//double angleAux = ayuda.anguloNecesario(goal, myRobotAPI, 0.01);
-				//double angle = ayuda.degToRad(ayuda.radToDeg(angleAux) + 180);
-				//myRobotAPI.setSteerHeading(angle);
-				
-				//if(myRobotAPI.alignedToBallandGoal())
-				//{
-					myRobotAPI.setDisplayString("Del. (Tiro)");
-					myRobotAPI.kick();					
-				//}	
-				
-				return myRobotAPI.ROBOT_OK;
-			}
-			
-			if (!ayuda.cercano(myRobotAPI.getPosition(), myRobotAPI.toFieldCoordinates(myRobotAPI.getOpponentsGoal()), myRobotAPI, 0.25))
-			{
+			case DetrasPelota:
 				myRobotAPI.setBehindBall(myRobotAPI.getOpponentsGoal());
-				return myRobotAPI.ROBOT_OK;
-			}
-			
-			if (ayuda.cercano(myRobotAPI.getPosition(), myRobotAPI.toFieldCoordinates(myRobotAPI.getOpponentsGoal()), myRobotAPI, 0.25)
-					&& ayuda.cercanoRadio(myRobotAPI.getPosition(),  myRobotAPI.toFieldCoordinates(myRobotAPI.getClosestOpponent()), myRobotAPI.getPlayerRadius()*2))
-			{
+				break;
+				
+			case PasaBola:
 				//Recorre donde estan todos sus compañeros y se la pasa al mas cercano
 				//que este en la zona de ataque
 				Vec2[] teammates=myRobotAPI.getTeammates();
@@ -77,46 +49,82 @@ public class Delantero extends Behaviour
 				
 				myRobotAPI.passBall(myRobotAPI.toFieldCoordinates(teammates[cercano]));
 				myRobotAPI.setDisplayString("Pasa balon");
-				return myRobotAPI.ROBOT_OK;
-			}
-			else
-			{
-				myRobotAPI.setBehindBall(myRobotAPI.getOpponentsGoal());
-				return myRobotAPI.ROBOT_OK;				
-			}
-		}
-		else
-		{
-			//Se queda en el medio esperando a que pase la bola
-			//Me quedo en el cuarto de campo esperando a algun contrario
-			//Evita bloquearse con compañeros
-			ayuda.evitaColision(myRobotAPI.getClosestMate(), myRobotAPI);
-			ayuda.evitaColision(myRobotAPI.getClosestOpponent(), myRobotAPI);
-			
-			//Debe esperar en el medio del campo
-			Double thirdSize = (myRobotAPI.getRightFieldBound() - myRobotAPI.getLeftFieldBound()) / 3;
-			// Check if the player is in the first third of the field
-			if (!(myRobotAPI.getPosition().x * myRobotAPI.getFieldSide() >= thirdSize))
-			{
-				// 1.- Set steering
-				Vec2 dest = myRobotAPI.getOpponentsGoal();
-				double angle = ayuda.anguloDestino(dest, myRobotAPI);			
-				myRobotAPI.setSteerHeading(angle);	
+				break;
 				
-				// 2.- Increase speed
-				myRobotAPI.setSpeed(1000);
+			case EsperarBola:
+				//Devuelve EsperarBola
+				//Se queda en el medio esperando a que pase la bola
+				//Me quedo en el cuarto de campo esperando a algun contrario
+				//Evita bloquearse con compañeros
+				ayuda.evitaColision(myRobotAPI.getClosestMate(), myRobotAPI);
+				ayuda.evitaColision(myRobotAPI.getClosestOpponent(), myRobotAPI);
+				
+				//Debe esperar en el medio del campo
+				Double thirdSize = (myRobotAPI.getRightFieldBound() - myRobotAPI.getLeftFieldBound()) / 3;
+				// Check if the player is in the first third of the field
+				if (!(myRobotAPI.getPosition().x * myRobotAPI.getFieldSide() >= thirdSize))
+				{
+					// 1.- Set steering
+					Vec2 dest = myRobotAPI.getOpponentsGoal();
+					double angle = ayuda.anguloDestino(dest, myRobotAPI);			
+					myRobotAPI.setSteerHeading(angle);	
+					
+					// 2.- Increase speed
+					myRobotAPI.setSpeed(1000);
 
-				// Set displayed text
-				myRobotAPI.setDisplayString("Del. (Wait)");
-				
-				// Return
-				return myRobotAPI.ROBOT_OK;
-			}
-			
+					// Set displayed text
+					myRobotAPI.setDisplayString("Del. (Wait)");
+				}
+				break;
+	
+			default:
+				break;
 		}
 		return myRobotAPI.ROBOT_OK;
 	}
 
+	private Estados calculaEstado()
+	{
+		//Cojo posicion de la bola
+		Vec2 ball=myRobotAPI.getBall();
+		Vec2 ballCoordinates=myRobotAPI.toFieldCoordinates(ball);
+		int defenseside=myRobotAPI.getFieldSide();
+		int ataqueside=(-1)*defenseside;
+
+		//Si la pelota esta en el lado donde se encuentra el delantero va a por la pelota
+		if( (ballCoordinates.x >= 0 && ataqueside==myRobotAPI.EAST_FIELD) || (ballCoordinates.x <= 0 && ataqueside==myRobotAPI.WEST_FIELD))
+		{
+			myRobotAPI.setDisplayString("Del. (PK)");
+			//Si puede golpearla, la golpea
+			//Sino prueba a pasarla
+			if(ayuda.cercano(myRobotAPI.getPosition(), myRobotAPI.toFieldCoordinates(myRobotAPI.getOpponentsGoal()), myRobotAPI, 0.25)
+					&& myRobotAPI.canKick() /*&& myRobotAPI.alignedToBallandGoal()*/
+					&& !ayuda.cercanoRadio(myRobotAPI.getPosition(),  myRobotAPI.toFieldCoordinates(myRobotAPI.getClosestOpponent()), myRobotAPI.getPlayerRadius()*2))
+			{
+				//Devuelve Golpear
+				return Estados.Golpear;			
+			}
+			
+			if (!ayuda.cercano(myRobotAPI.getPosition(), myRobotAPI.toFieldCoordinates(myRobotAPI.getOpponentsGoal()), myRobotAPI, 0.25))
+			{
+				//Devuelve detrasPelota
+				return Estados.DetrasPelota;
+			}
+			
+			if (ayuda.cercano(myRobotAPI.getPosition(), myRobotAPI.toFieldCoordinates(myRobotAPI.getOpponentsGoal()), myRobotAPI, 0.25)
+					&& ayuda.cercanoRadio(myRobotAPI.getPosition(),  myRobotAPI.toFieldCoordinates(myRobotAPI.getClosestOpponent()), myRobotAPI.getPlayerRadius()*2))
+			{
+				//Devuelve pasaBola
+				return Estados.PasaBola;
+			}
+			else
+			{
+				return Estados.DetrasPelota;
+			}
+		}
+		//Por defecto espera la bola
+		return Estados.EsperarBola;
+	}
 	public void onInit(RobotAPI r) 
 	{
 	}
--- ISBCTournament/ISBCTournament/src/AstonBirrasFc/Pepe.java
+++ ISBCTournament/ISBCTournament/src/AstonBirrasFc/Pepe.java
@@ -9,18 +9,78 @@ import EDU.gatech.cc.is.util.Vec2;
 public class Pepe extends Behaviour
 {
 	private Ayudas ayudas;
+	private enum Estados {BloquearRival,EsperarRival};
+	private Estados estado;
+	private ArrayList<Integer>  cerca;
 	
 	public void configure() 
 	{ 
 		ayudas = new Ayudas();
+		cerca=new ArrayList<Integer>();
 	}
 
 	public int takeStep() 
 	{	
+		estado=calculaEstado();
+		
+		Vec2[] opponents=myRobotAPI.getOpponents();
+		
+		switch (estado) {
+			case BloquearRival:
+				double distanciaComparar=Double.MAX_VALUE;
+				int robotCerca=0;
+				for(int i=0;i< cerca.size();i++)
+				{
+					Vec2 posicionContrario=opponents[cerca.get(i)];
+					double distancia=ayudas.distanciaEntre(myRobotAPI.getPosition(), posicionContrario);
+					if(distancia < distanciaComparar)
+					{
+						distanciaComparar=distancia;
+						robotCerca=cerca.get(i);
+					}
+				}
+				
+				//En este punto tengo el robot que esta a menos distancia y en mi area
+				//Giro el centro hacia el he intento ir a bloquearlo
+				double angulo=ayudas.anguloDestino(opponents[robotCerca], myRobotAPI);
+				myRobotAPI.setSteerHeading(angulo);
+				myRobotAPI.setSpeed(1.0);
+				myRobotAPI.setDisplayString("Pepe(Bloq)");
+				break;
+			case EsperarRival:
+				//Me quedo en el cuarto de campo esperando a algun contrario
+				//Evita bloquearse con compañeros
+				ayudas.evitaColision(myRobotAPI.getClosestMate(), myRobotAPI);
+				
+				//Coje los tres cuartos de campo y se queda cerca de ese punto
+				Double thirdSize = (myRobotAPI.getRightFieldBound() - myRobotAPI.getLeftFieldBound()) / 3;
+				// Check if the player is in the first third of the field
+				if (!(myRobotAPI.getPosition().x * myRobotAPI.getFieldSide() >= thirdSize))
+				{
+					// 1.- Set steering
+					Vec2 dest = myRobotAPI.getOurGoal();
+					double angle = ayudas.anguloDestino(dest, myRobotAPI);			
+					myRobotAPI.setSteerHeading(angle);	
+					
+					// 2.- Increase speed
+					myRobotAPI.setSpeed(1000);
+
+					// Set displayed text
+					myRobotAPI.setDisplayString("Pepe (Wait)");
+				}
+				break;
+			default:
+				break;
+		}
+		 
+		return myRobotAPI.ROBOT_OK;
+	}
+
+	private Estados calculaEstado()
+	{
 		//Cojo donde estan los jugadores del equipo contrario
 		int side=myRobotAPI.getFieldSide();
 		Vec2[] opponents=myRobotAPI.getOpponents();
-		ArrayList<Integer> cerca=new ArrayList<Integer>();
 		for(int i=0; i< opponents.length;i++)
 		{
 			Vec2 opponent=opponents[i];
@@ -52,56 +112,13 @@ public class Pepe extends Behaviour
 		//Sino me quedo en el medio esperando y evitando contrarios
 		if(!cerca.isEmpty())
 		{
-			double distanciaComparar=Double.MAX_VALUE;
-			int robotCerca=0;
-			for(int i=0;i< cerca.size();i++)
-			{
-				Vec2 posicionContrario=opponents[cerca.get(i)];
-				double distancia=ayudas.distanciaEntre(myRobotAPI.getPosition(), posicionContrario);
-				if(distancia < distanciaComparar)
-				{
-					distanciaComparar=distancia;
-					robotCerca=cerca.get(i);
-				}
-			}
-			//En este punto tengo el robot que esta a menos distancia y en mi area
-			//Giro el centro hacia el he intento ir a bloquearlo
-			double angulo=ayudas.anguloDestino(opponents[robotCerca], myRobotAPI);
-			myRobotAPI.setSteerHeading(angulo);
-			myRobotAPI.setSpeed(1.0);
-			myRobotAPI.setDisplayString("Pepe(Bloq)");
-			return myRobotAPI.ROBOT_OK;
+			//devuelve bloquearRival
+			return Estados.BloquearRival;
 		}
-		else
-		{
-			//Me quedo en el cuarto de campo esperando a algun contrario
-			//Evita bloquearse con compañeros
-			ayudas.evitaColision(myRobotAPI.getClosestMate(), myRobotAPI);
-			
-			//Coje los tres cuartos de campo y se queda cerca de ese punto
-			Double thirdSize = (myRobotAPI.getRightFieldBound() - myRobotAPI.getLeftFieldBound()) / 3;
-			// Check if the player is in the first third of the field
-			if (!(myRobotAPI.getPosition().x * myRobotAPI.getFieldSide() >= thirdSize))
-			{
-				// 1.- Set steering
-				Vec2 dest = myRobotAPI.getOurGoal();
-				double angle = ayudas.anguloDestino(dest, myRobotAPI);			
-				myRobotAPI.setSteerHeading(angle);	
-				
-				// 2.- Increase speed
-				myRobotAPI.setSpeed(1000);
-
-				// Set displayed text
-				myRobotAPI.setDisplayString("Pepe (Wait)");
-				
-				// Return
-				return myRobotAPI.ROBOT_OK;
-			}
-		}
-		 
-		return myRobotAPI.ROBOT_OK;
+		
+		//Devuelve el por defecto que es esperar
+		return Estados.EsperarRival;
 	}
-
 	public void onInit(RobotAPI r) 
 	{
 	}
